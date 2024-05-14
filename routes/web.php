29a019366411177e219d3d45bcf11b284cacfa20
Majumder99 <souravmajumderk99@gmail.com>
2024-05-14 11:08:10 +0600

    difference between web.php and api.php

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,9 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return "I'm sourav";
+});
+
+Route::get("/get", function () {
+    return "This is a post request";
 });
